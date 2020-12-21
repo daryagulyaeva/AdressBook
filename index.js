fd0ba4d0302545cb4cc2bff4bb7cf131f777e13e
daryagulyaeva <darya_gulyaeva2011@mail.ru>
2020-12-21 22:03:05 +0300

    started creating adressBook

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("open").onclick=function(){
+let modalAdd=document.getElementById("modal-add");
+modalAdd.style.display="flex";
+}
+document.getElementById("close").onclick=function(){
+    let modalAdd=document.getElementById("modal-add");
+    modalAdd.style.display="none";
+    }
